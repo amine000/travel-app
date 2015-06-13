79bf08701cfba839a676562db7381ba7e34d69b6
97agupta <aman@Amans-MacBook-Air.local>
2015-06-13 14:43:20 -0700

    Added Toast :) :) :)

--- app/src/main/java/com/example/amine_000/test_application/MainActivity.java
+++ app/src/main/java/com/example/amine_000/test_application/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.amine_000.test_application;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -36,4 +39,15 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void findGuides(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
 }
