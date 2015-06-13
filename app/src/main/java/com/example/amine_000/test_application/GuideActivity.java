db1e897a0039ed673e172103e406934a6ac32606
97agupta <aman@Amans-MacBook-Air.local>
2015-06-13 15:17:38 -0700

    now its hello world!!

--- app/src/main/java/com/example/amine_000/test_application/GuideActivity.java
+++ app/src/main/java/com/example/amine_000/test_application/GuideActivity.java
@@ -1,26 +1,24 @@
 package com.example.amine_000.test_application;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class GuideActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_guide);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_guide, menu);
         return true;
     }
 
@@ -38,11 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void findGuides(View view){
-        Intent intent = new Intent(this, GuideActivity.class);
-        startActivity(intent);
-    }
-
-
 }
---
+++ app/src/main/java/com/example/amine_000/test_application/GuideActivityFragment.java
@@ -0,0 +1,23 @@
+package com.example.amine_000.test_application;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class GuideActivityFragment extends Fragment {
+
+    public GuideActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_guide, container, false);
+    }
+}
